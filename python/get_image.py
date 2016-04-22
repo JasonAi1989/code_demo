6edf1fd2f9d8c877f7450e8b0c264ea625b6c7fc
Jason Ai <aizhaoyu@163.com>
2016-04-22 11:21:14 +0800

    sd
    Signed-off-by: Jason Ai <aizhaoyu@163.com>

--- python/get_image.py
+++ python/get_image.py
@@ -5,12 +5,28 @@ import os.path
 
 def get_image_data(addr):
     response=urllib.request.urlopen(addr)
+    if response.getcode() != 200:
+        return None
+
     html=response.read()
+
+    if len(html) == 0:
+        return None
+    
     return html
 
+def analysis_data(data):
+    data_string=str(data)
+    find='http://404.safedog.cn'
+    p=data_string.find(find)
+    if p == -1:
+        return 0
+    
+    return -1
+
+    
 def save_image_to_local(data, path, file_name):
     if os.path.isdir(path) == False:
-        print('h')
         os.makedirs(path)
 
     name=path+'/'+file_name
@@ -23,15 +39,30 @@ base_url='http://tu.qzcpic.com/1/'
 base_path='animation/'
 
 count=0
-for i in range(1, 400):
-    for j in range(1, 400):
+bad_page=0
+max_bad_page=50
+
+for i in range(1, 1000):
+    for j in range(1, 1000):
+        if bad_page == max_bad_page:
+            bad_page = 0
+            break
+        
         url=base_url+str(i)+'/'+str(j)+'.jpg'
         path=base_path+str(i)
         file_name=str(j)+'.jpg'
 
         data=get_image_data(url)
-        save_image_to_local(data, path, file_name)
+        if data == None:
+            bad_page += 1
+            continue
         
-        count = count + 1
-        print('count: %d', count)
+        if analysis_data(data) == -1:
+            bad_page += 1
+            continue
+        
+        save_image_to_local(data, path, file_name)
+        count += 1
+
+    print('dir ', i, ' count: ', count)
 
